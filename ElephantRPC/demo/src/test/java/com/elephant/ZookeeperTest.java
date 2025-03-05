e8d405d6f684261de263506b801cea3bf99d124a
Elephant- BIG-LEG <zeyongfang4@gmail.com>
2025-03-05 16:48:47 +0800

    接入 ZooKeeper 并测试成功

---
+++ ElephantRPC/demo/src/test/java/com/elephant/ZookeeperTest.java
@@ -0,0 +1,45 @@
+package com.elephant;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * @Author: Elephant-FZY
+ * @Email: https://github.com/Elephant-BIG-LEG
+ * @Date: 2025/03/05/16:08
+ * @Description: TODO
+ */
+public class ZookeeperTest {
+
+    ZooKeeper zooKeeper;
+    public void createZK(){
+        String connectString = "127.0.0.1:2181";
+        int timeOut = 10000;
+        try {
+            zooKeeper = new ZooKeeper(connectString,timeOut,null);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testCreateZK(){
+        createZK();
+        try {
+            String result = zooKeeper.create("/ElephantRPC", "hello".getBytes(),
+                    ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            System.out.println("result: " + result);
+        } catch (KeeperException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
